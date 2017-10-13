c8ad3c357346446876a402f99d6ff15b28a81ece
Stanley Mwangi <stanleym2711@gmail.com>
2017-10-13 01:49:56 +0100

    Clean up training data set

--- script.py
+++ script.py
@@ -8,3 +8,25 @@ print(titanic.head(5))
 
 # Show summary stats for numeric columns
 print(titanic.describe())
+
+# Age column has missing values as count is not 891
+# Replace missing ages with median age
+titanic["Age"] = titanic["Age"].fillna(titanic["Age"].median())
+
+# Gender is currently non-numeric but could be useful in predictions
+print(titanic["Sex"].unique())
+
+# Convert male to 0 and female to 1
+titanic.loc[titanic["Sex"] == "male", "Sex"] = 0
+titanic.loc[titanic["Sex"] == "female", "Sex"] = 1
+
+# Show current unique values in embarked column
+print(titanic["Embarked"].unique())
+
+# Replace missing values with most common port which is S
+titanic["Embarked"] = titanic["Embarked"].fillna("S")
+
+# Assign numbers to S, C and Q
+titanic.loc[titanic["Embarked"] == "S", "Embarked"] = 0
+titanic.loc[titanic["Embarked"] == "C", "Embarked"] = 1
+titanic.loc[titanic["Embarked"] == "Q", "Embarked"] = 2
