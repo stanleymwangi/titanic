ff7c30372933e3b7e469c27279f7d49a61242e00
Stanley Mwangi <stanleym2711@gmail.com>
2017-10-13 03:53:02 +0100

    Calculate accuracy

--- script.py
+++ script.py
@@ -1,5 +1,6 @@
 # Import libraries for analysis
 import pandas as pd
+import numpy as np
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import KFold # helper for cross validation
 
@@ -45,7 +46,6 @@ kf = KFold(n_splits=3, random_state=1)  # setting random_state ensures splits st
 
 predictions = []
 for train, test in kf.split(titanic): # kf.split produces row indices for training and testing
-    count +=1
 
     # Predictors for training the algorithm from the training fold
     train_predictors = (titanic[predictors].iloc[train,:])
@@ -57,7 +57,20 @@ for train, test in kf.split(titanic): # kf.split produces row indices for traini
     linreg_algorithm.fit(train_predictors, train_target)
 
     # Apply trained algorithm to test fold to make predictions
-    test_predictions = linreg_algorithm.fit(titanic[predictors].iloc[test,:])
+    test_predictions = linreg_algorithm.predict(titanic[predictors].iloc[test,:])
 
     # Add resulting predictions
     predictions.append(test_predictions)
+
+# Evaluate error
+# Combine numpy arrays which hold our separate predictions into one column
+predictions = np.concatenate(predictions, axis=0)
+
+# Map predictions to 0 or 1
+predictions[predictions > 0.5] = 1
+predictions[predictions <= 0.5] = 0
+
+# Error metric is the percentage of correct predictions
+accuracy = round(np.sum(predictions == titanic["Survived"]) / titanic.shape[0] * 100, 1)
+
+print(accuracy)
