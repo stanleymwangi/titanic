f9933065860b2dd5245d934713c0b963d6275e77
Stanley Mwangi <stanleym2711@gmail.com>
2017-10-13 05:10:52 +0100

    Remove redundant data preparation from logistic regression model

--- logistic_regression.py
+++ logistic_regression.py
@@ -4,23 +4,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import cross_val_score
 
 # Create dataframe from training data set
-titanic = pd.read_csv("train.csv")
-
-# Age column has missing values as count is not 891
-# Replace missing ages with median age
-titanic["Age"] = titanic["Age"].fillna(titanic["Age"].median())
-
-# Convert male to 0 and female to 1
-titanic.loc[titanic["Sex"] == "male", "Sex"] = 0
-titanic.loc[titanic["Sex"] == "female", "Sex"] = 1
-
-# Replace missing values with most common port which is S
-titanic["Embarked"] = titanic["Embarked"].fillna("S")
-
-# Assign numbers to S, C and Q
-titanic.loc[titanic["Embarked"] == "S", "Embarked"] = 0
-titanic.loc[titanic["Embarked"] == "C", "Embarked"] = 1
-titanic.loc[titanic["Embarked"] == "Q", "Embarked"] = 2
+titanic = pd.read_csv("clean_train.csv")
 
 # Columns for target prediction
 predictors = ["Pclass", "Sex", "Age", "SibSp", "Parch", "Fare", "Embarked"]
